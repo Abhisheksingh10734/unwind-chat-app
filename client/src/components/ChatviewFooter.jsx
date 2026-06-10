b780f98857e59206cc5c64865ebe8e3c0733a5ad
Abhishek Singh <abhisingh10734@gmail.com>
2026-06-11 01:26:15 +0530

    feat: messagign using socket.io implemented

--- client/src/components/ChatviewFooter.jsx
+++ client/src/components/ChatviewFooter.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRef } from "react";
 
-export const ChatviewFooter = () => {
+export const ChatviewFooter = ({setData, socket}) => {
     const textareaRef = useRef(null);
-    
-      const handleInput = (e) => {
+    const [message, setMessage] = useState("");
+
+    const handleInput = (e) => {
         e.target.style.height = "auto";
         e.target.style.height = `${e.target.scrollHeight}px`;
-      };
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (!message.trim()) return;
+        
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+        }
+        
+        socket.emit("message", {
+            text: message,
+            senderId: socket.id
+        });
+        setMessage("");
+    };
+    
     return (
         <>
             <div className="w-full bg-[#2D2A40] fixed bottom-0 left-0 flex items-end gap-3 px-3 py-2">
@@ -16,23 +34,30 @@ export const ChatviewFooter = () => {
                     🗃️
                 </div>
 
-                <div className="bg-[#1E1B2E] flex-1 rounded-lg px-3 py-2 flex items-center justify-center">
-                    <textarea
-                        ref={textareaRef}
-                        rows="1"
-                        onInput={handleInput}
-                        placeholder="Type a message..."
-                        className="w-full bg-transparent outline-none resize-none text-white placeholder-gray-400 max-h-32 overflow-y-auto"
-                    />
-                </div>
+                <form onSubmit={handleSubmit} className='flex gap-4 flex-1'>
+                    <div className="bg-[#1E1B2E] flex-1 rounded-lg px-3 py-2 flex items-center justify-center">
+                        <textarea
+                            ref={textareaRef}
+                            value={message}
+                            rows="1"
+                            onInput={handleInput}
+                            onChange={(e) => setMessage(e.target.value)}
+                            placeholder="Type a message..."
+                            className="w-full scrollbar-none bg-transparent outline-none resize-none text-white placeholder-gray-400 max-h-32 overflow-y-auto"
+                        />
+                    </div>
+
+                    <button
+                        type='submit'
+                        className="bg-[#1E1B2E] p-2 rounded-lg cursor-pointer">
+                        📤
+                    </button>
+                </form>
 
                 <div className="bg-[#1E1B2E] p-2 rounded-lg">
                     😊
                 </div>
 
-                <button className="bg-[#1E1B2E] p-2 rounded-lg">
-                    📤
-                </button>
 
             </div>
         </>
