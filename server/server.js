161cd6f40b0071160a3e8405f331f3a715664a58
Abhishek Singh <abhisingh10734@gmail.com>
2026-06-10 18:53:46 +0530

    feat: server setup

---
+++ server/server.js
@@ -0,0 +1,34 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected:", socket.id);
+
+  socket.on("message", (msg) => {
+    console.log("Received:", msg);
+
+    io.emit("message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
