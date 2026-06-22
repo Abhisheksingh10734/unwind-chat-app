d2bfdb28ed90c7b5fbf7a6f580d0c7347c1a98ed
Abhishek Singh <abhisingh10734@gmail.com>
2026-06-23 00:55:56 +0530

    client socket context add

---
+++ uniwnd_v2/client/src/context/SocketContext.jsx
@@ -0,0 +1,39 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { io } from "socket.io-client";
+
+const SocketContext = createContext();
+
+export const SocketProvider = ({ children }) => {
+    const [socket, setSocket] = useState(null);
+
+    useEffect(() => {
+        const socketInstance = io("http://localhost:5000", {
+            withCredentials: true,
+            transports: ["websocket"],
+        });
+
+        setSocket(socketInstance);
+
+        socketInstance.on("connect", () => {
+            console.log("Socket Connected:", socketInstance.id);
+        });
+
+        socketInstance.on("disconnect", () => {
+            console.log("Socket Disconnected");
+        });
+
+        return () => {
+            socketInstance.disconnect();
+        };
+    }, []);
+
+    return (
+        <SocketContext.Provider value={{ socket }}>
+            {children}
+        </SocketContext.Provider>
+    );
+};
+
+export const useSocket = () => {
+    return useContext(SocketContext);
+};
