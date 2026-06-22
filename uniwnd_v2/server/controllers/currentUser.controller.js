c2fc4a8886c0b7c92359a17bacbf32a68ad0756b
Abhishek Singh <abhisingh10734@gmail.com>
2026-06-22 23:13:30 +0530

    feat: currentUser controller updated

--- uniwnd_v2/server/controllers/currentUser.controller.js
+++ uniwnd_v2/server/controllers/currentUser.controller.js
@@ -4,17 +4,36 @@ export const currentUser = async (req, res) => {
     try {
         const userId = req.user.id;
 
-        if(userId) {
+        if(!userId) {
             return res.status(500).json({
                 success: false,
                 message: "No user found."
             });
         }
 
-        const user = await db.query("SELECT id, username, email, avatar_public_url from unwind_users where id = $1", [userId]);
+        const user = await db.query("SELECT id, username, email, avatar from unwind_users where id = $1", [userId]);
+
+        if(user.rowCount === 0) {
+            return res.status(500).json({
+                success: false,
+                message: "No user found."
+            });
+        }
+
+        const currentUserId = user.rows[0].id;
+        const currentUserEmail = user.rows[0].email;
+        const currentUserAvatar = user.rows[0].avatar;
+        const currentUserUsername = user.rows[0].username;
+        
+        
     
     return res.status(200).json({
-        message: "getting current user"
+        success: true,
+        currentUserId,
+        currentUserUsername,
+        currentUserEmail,
+        currentUserAvatar,
+        message: "User fetched successfully."
     });
     } catch (error) {
         
